19f87b8812498f7c732fd58fb9c42e53276d2357
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-10 17:35:29 +0200

    add package.json with npm init

--- test/test.js
+++ test/test.js
@@ -5,12 +5,16 @@ describe('Song', function() {
       let synthwave;
       before(() => {
         chordA = new Chord("A", "assets/chords/A.wav")
-        synthwave = new Song("Synthwave", [chord], 1)
+        ChordB = new Chord("B", "assets/chords/B.wav")
+        chords = []
+        chords.push(chordA)
+        chords.push(ChordB)
+        synthwave = new Song("Synthwave", chords, 1)
       })
       it('can create a BoardMember with a name, home state, and training', function() {
         expect(synthwave).to.be.an.instanceof(Song)
         expect(synthwave.name).to.equal("Synthwave")
-        expect(synthwave.chords).to.equal([chord])
+        expect(synthwave.chords).to.equal(chords)
         expect(synthwave.id).to.equal(1)
       })
     })
