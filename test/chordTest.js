fcf13b69d74820d49cbad0254fa5e11c9869b4d9
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-11 12:30:31 +0200

    develop chord test with more chai.expect statements

--- test/chordTest.js
+++ test/chordTest.js
@@ -1,16 +1,19 @@
 const assert = chai.assert;
 const expect = chai.expect;
-let chord, edit_id;
+let chord, edit_id, audio;
 
 
 describe("Chord", () => {
   beforeEach(() => {
     chord = new Chord("A", "assets/chords/A.wav")
-    edit_id = chord.edit_id
+    audio = chord.audio()
   })
 
   it("has a class of Chord", () => {
-    expect(new Chord()).to.be.an.instanceof(Chord);
+    expect(chord).to.be.an.instanceof(Chord);
+    expect(chord).to.have.property('name')
+    expect(chord).to.have.property('file')
+    expect(chord).to.have.property('edit_id')
   })
   it("The chord name is returned", () => {
     expect(chord.name).to.eq('A')
@@ -19,6 +22,10 @@ describe("Chord", () => {
     expect(chord.file).to.eq('assets/chords/A.wav')
   })
   it("The chord random edit_id exists", () => {
-    expect(chord.edit_id).to.eq(edit_id)
+    expect(chord.edit_id).to.be.a('number')
   })
+  it("The chord audio has correct source", () => {
+    expect(audio.src).to.eq("file:///Users/santiago/Development/code/chords_beats_frontend/test/assets/chords/A.wav")
+  })
+
 })
