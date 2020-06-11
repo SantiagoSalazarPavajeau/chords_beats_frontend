6794b6bf1b6b59d8c235b9a8d78c7022c13d217e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-11 12:02:26 +0200

    add chord test

--- test/chordTest.js
+++ test/chordTest.js
@@ -1,17 +1,24 @@
 const assert = chai.assert;
 const expect = chai.expect;
-let chord;
+let chord, edit_id;
 
 
 describe("Chord", () => {
   beforeEach(() => {
     chord = new Chord("A", "assets/chords/A.wav")
+    edit_id = chord.edit_id
   })
 
   it("has a class of Chord", () => {
     expect(new Chord()).to.be.an.instanceof(Chord);
   })
-  it("chord.name returns name", () => {
+  it("The chord name is returned", () => {
     expect(chord.name).to.eq('A')
+  })
+  it("The chord file directory is returned", () => {
+    expect(chord.file).to.eq('assets/chords/A.wav')
+  })
+  it("The chord random edit_id exists", () => {
+    expect(chord.edit_id).to.eq(edit_id)
   })
 })
