5658adc36d98c14f1edba90032edfd2a258d352e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-11 13:50:41 +0200

    remove npm

---
+++ test/songTest.js
@@ -0,0 +1,22 @@
+// let song, chords;
+
+
+// describe("Song", () => {
+//   beforeEach(() => {
+//     song = new Song("A", "assets/chords/A.wav")
+//     edit_id = chord.edit_id
+//   })
+
+//   it("has a class of Song", () => {
+//     expect(new Song()).to.be.an.instanceof(Song);
+//   })
+//   it("The song name is returned", () => {
+//     expect(song.name).to.eq('A')
+//   })
+//   it("The song chords array is returned", () => {
+//     expect(song.chords).to.eq([chord1,chord2])
+//   })
+//   it("The chord random edit_id exists", () => {
+//     expect(chord.edit_id).to.eq(edit_id)
+//   })
+// })
