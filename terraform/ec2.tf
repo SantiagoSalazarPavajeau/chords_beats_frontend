9c843d34558cb4c8b398747ce9be70736cbdfc96
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2024-09-18 15:00:53 -0400

    configure security groups and subnets on hosts

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,18 @@
+resource "aws_instance" "web_server" {
+  ami           = var.aws_instance_ami
+  instance_type = var.aws_instance_type
+  subnet_id = module.vpc.public_subnets
+  vpc_security_group_ids = [module.public_sg.security_group_id]
+  tags = {
+    Name = "ExampleWebServerInstance"
+  }
+}
 resource "aws_instance" "app_server" {
   ami           = var.aws_instance_ami
   instance_type = var.aws_instance_type
+  subnet_id = module.vpc.private_subnets
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+
 
   tags = {
     Name = "ExampleAppServerInstance"
@@ -10,6 +22,9 @@ resource "aws_instance" "app_server" {
 resource "aws_instance" "nat" {
   ami           = var.aws_instance_ami
   instance_type = var.aws_instance_type
+  subnet_id = module.vpc.public_subnets
+  vpc_security_group_ids = [module.nat_sg.security_group_id]
+
   tags = {
     Name = "NAT Instance"
   }
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -7,15 +7,7 @@ module "public_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["203.0.113.0/24"]
-    }
-  ]
+  ingress_rules       = ["ssh-tcp"]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
@@ -29,7 +21,7 @@ module "nat_sg" {
   description = "Allow traffic from private subnet"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["10.0.1.0/24"]
+  ingress_cidr_blocks = ["10.0.2.0/24"]
   ingress_rules       = ["all-all"]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
@@ -44,22 +36,8 @@ module "private_sg" {
   description = "Allow traffic from public subnet and NAT instance"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["10.0.0.0/24"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["10.0.0.0/24"]
-    }
-  ]
-  ingress_with_source_security_group_id = [
-    {
-      rule                     = "all-all"
-      source_security_group_id = module.nat_sg.this_security_group_id
-    }
-  ]
+  ingress_cidr_blocks = ["10.0.1.0/24"]
+  ingress_rules       = ["all-all"]
 
   egress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
