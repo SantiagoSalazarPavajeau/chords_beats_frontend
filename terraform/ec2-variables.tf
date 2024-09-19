4e612498cceec0c09e5527d2c530a524f758ec24
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2024-09-19 18:55:01 -0400

    add eip

--- terraform/ec2-variables.tf
+++ terraform/ec2-variables.tf
@@ -9,3 +9,8 @@ variable "aws_instance_type" {
   type = string
   default = "t2.micro"
 }
+variable "instance_keypair" {
+  description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
+  type = string
+  default = "eks-terraform-key"
+}
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,16 +1,29 @@
-resource "aws_instance" "web_server" {
-  ami           = var.aws_instance_ami
-  instance_type = var.aws_instance_type
-  subnet_id = module.vpc.public_subnets
+# resource "aws_instance" "web_server" {
+#   ami           = var.aws_instance_ami
+#   instance_type = var.aws_instance_type
+#   subnet_id = module.vpc.public_subnets[0]
+#   vpc_security_group_ids = [module.public_sg.security_group_id]
+#   tags = {
+#     Name = "ExampleWebServerInstance"
+#   }
+# }
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.0"
+  name = "${local.name}-WebServerHost"
+  ami                    = var.aws_instance_ami
+  instance_type          = var.aws_instance_type
+  key_name               = var.instance_keypair
+  #monitoring             = true
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.public_sg.security_group_id]
-  tags = {
-    Name = "ExampleWebServerInstance"
-  }
+  
+  tags = local.common_tags
 }
 resource "aws_instance" "app_server" {
   ami           = var.aws_instance_ami
   instance_type = var.aws_instance_type
-  subnet_id = module.vpc.private_subnets
+  subnet_id = module.vpc.private_subnets[0]
   vpc_security_group_ids = [module.private_sg.security_group_id]
 
 
@@ -22,7 +35,7 @@ resource "aws_instance" "app_server" {
 resource "aws_instance" "nat" {
   ami           = var.aws_instance_ami
   instance_type = var.aws_instance_type
-  subnet_id = module.vpc.public_subnets
+  subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.nat_sg.security_group_id]
 
   tags = {
